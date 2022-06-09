5aa8ecd123d449eff6002085d37f45aff1d5e9c0
Fidan1243 <fidan.imanova.87@gmail.com>
2022-06-09 11:51:53 +0400

    2 files added

---
+++ src/app/models/news.repository.ts
@@ -0,0 +1,38 @@
+import { News } from './news';
+
+export class NewsRepository {
+  private news_array: News[];
+
+  constructor() {
+    this.news_array = [
+      {
+        Id: 1,
+        MovieId: 1,
+        title: 'New Record',
+        authors: 'John',
+        CreatedDate: new Date(),
+      },
+      {
+        Id: 2,
+        MovieId: 2,
+        title: 'The best drama',
+        authors: 'Jordan',
+        CreatedDate: new Date(),
+      },
+      {
+        Id: 3,
+        MovieId: 2,
+        title: 'The best film of the year',
+        authors: 'Michael',
+        CreatedDate: new Date(),
+      },
+      {
+        Id: 4,
+        MovieId: 3,
+        title: 'The best historical film',
+        authors: 'Thomas',
+        CreatedDate: new Date(),
+      },
+    ];
+  }
+}
